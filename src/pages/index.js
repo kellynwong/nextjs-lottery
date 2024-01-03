37b3caf911906f3190ec581510797f489feaf063
kellyn <kellynwong@hotmail.com>
2024-01-04 02:25:37 +0800

    Create Manual Header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import Head from "next/head";
+import ManualHeader from "/components/ManualHeader";
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Smart Contract Lottery</title>
-        <meta name="description" content="Our Smart Contract Lottery" />
+        <meta name="description" content="Our Smart Contract Lottery" />{" "}
+        {/*content will be read by webscrapers*/}
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <ManualHeader />
+      {/*header / connect button / nav bar*/}
       Hello!
     </div>
   );
