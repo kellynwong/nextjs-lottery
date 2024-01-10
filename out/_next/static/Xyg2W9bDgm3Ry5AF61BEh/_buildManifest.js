26cf9d978601700987d3a36a61f80e353f534e02
kellyn <kellynwong@hotmail.com>
2024-01-11 00:36:59 +0800

    Update for Fleek deployment

---
+++ out/_next/static/Xyg2W9bDgm3Ry5AF61BEh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-4ff835c8f5a12057.js"],"/_error":["static/chunks/pages/_error-c3101dbfeee25159.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
