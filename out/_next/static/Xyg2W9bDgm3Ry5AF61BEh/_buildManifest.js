b3cb923b7003fe3763341cbfa5a1da7ec8ed51f0
kellyn <kellynwong@hotmail.com>
2024-01-11 00:51:50 +0800

    Revert changes

--- out/_next/static/Xyg2W9bDgm3Ry5AF61BEh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-4ff835c8f5a12057.js"],"/_error":["static/chunks/pages/_error-c3101dbfeee25159.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
